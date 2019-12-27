7a306f9f9d1ee104f9a576c9fe1b2c822379d50e
Lucas Natoli <lucas.natoli@gmail.com>
2019-12-27 11:25:29 -0300

    child parse warning fixed

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -37,42 +37,40 @@ class Search extends React.Component {
     }
     const header = results ? results.info : 'none header'
     const { Search } = Input
+    const content = <div>
+      <Row type="flex" justify="space-around" align="middle" className="big-search">
+        <Col span={22} className="big-search-col">
+          <h1 className="search-title">Seed Finder</h1>
+          <Search size="large" placeholder="search strains and breeders" onSearch={query => this.searchStrain(query)} enterButton />
+        </Col>
+      </Row>
+      <Row>
+        <Col span={22} offset={1} className="search-results">
+          <List
+            header={header}
+            bordered
+            dataSource={strains}
+            loading={fetching}
+            pagination={{
+              onChange: page => {
+                console.log(page);
+              },
+              pageSize: 6,
+            }}
+            renderItem={item => (
+              <List.Item>
+                <List.Item.Meta
+                  title={<a href={"strain/" + item.id + "/" + item.brid}>{item.name}</a>}
+                  description={'Breeder: ' + item.brname} />
+              </List.Item>
+            )}
+          >
+          </List>
+        </Col>
+      </Row>
+    </div>
     return (
-      
-      <BaseLayout title="Search" menu="search" {...this.props} content={
-        <div>
-          <Row type="flex" justify="space-around" align="middle" className="big-search">
-            <Col span={22} className="big-search-col">
-              <h1 className="search-title">Seed Finder</h1>
-              <Search size="large" placeholder="search strains and breeders" onSearch={query => this.searchStrain(query)} enterButton />
-            </Col>
-          </Row>
-          <Row>
-            <Col span={22} offset={1} className="search-results">
-              <List
-                header={header}
-                bordered
-                dataSource={strains}
-                loading={fetching}
-                pagination={{
-                  onChange: page => {
-                    console.log(page);
-                  },
-                  pageSize: 6,
-                }}
-                renderItem={item => (
-                  <List.Item>
-                    <List.Item.Meta
-                      title={<a href={"strain/" + item.id + "/" + item.brid}>{item.name}</a>}
-                      description={'Breeder: ' + item.brname} />
-                  </List.Item>
-                )}
-              >
-              </List>
-            </Col>
-          </Row>
-        </div>
-      } />
+      <BaseLayout title="Search" menu="search" {...this.props} content={content} />
     )
   }
 }
